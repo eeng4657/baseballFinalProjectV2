cd9bb9904062d92cdb3e207f50b3e548509c126c
eeng4657 <eeng4657@bths.edu>
2023-05-31 12:12:43 -0400

    components of game made, might need header files

--- baseballGame/Pitcher.cpp
+++ baseballGame/Pitcher.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include "Ball.cpp"
+using namespace std;
 
-int main()
-{
-  std::cout << "Hello World!";
-}
+class Pitcher {
+  string firstName;
+  string lastName;
+  int accuracy;
+  int stability;
+  int power;
+  float era;
+  int walksAllowed;
+  int homeRunsAllowed;
+  Ball currentPitch;
+};
 
 //https://cplusplus.com/doc/tutorial/
+//https://stackoverflow.com/questions/12733888/regarding-c-include-another-class

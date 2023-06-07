b3c301dd5b76d45c0b1edc661ab7bc0b34d17e7f
eeng4657 <eeng4657@bths.edu>
2023-06-06 23:36:24 -0400

    resources for 3d render

--- baseballGame/Pitcher.cpp
+++ baseballGame/Pitcher.cpp
@@ -20,3 +20,7 @@ class Pitcher {
 //https://stackoverflow.com/questions/12733888/regarding-c-include-another-class
 //https://www.learncpp.com/
 //https://www.simplilearn.com/tutorials/cpp-tutorial/guide-to-understand-cpp-header-files
+//https://www.geeksforgeeks.org/templates-cpp/#
+//https://www.3dgep.com/beginning-cpp-template-programming/#more-9892
+//https://www.gamedeveloper.com/blogs/how-to-make-your-own-c-game-engine
+//https://www.google.com/search?q=GLFW&rlz=1C1VDKB_enUS980US980&sourceid=chrome&ie=UTF-8
